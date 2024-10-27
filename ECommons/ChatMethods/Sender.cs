afddcc42b0314e636b1ff0472962d62d8147c4fb
MeowZ#WR <antabakala@hotmail.com>
2024-10-27 15:19:47 +0800

    等不及了。

--- ECommons/ChatMethods/Sender.cs
+++ ECommons/ChatMethods/Sender.cs
@@ -1,10 +1,13 @@
-﻿using Dalamud.Game.ClientState.Objects.SubKinds;
-using Dalamud.Game.Text.SeStringHandling;
-using ECommons.DalamudServices;
-using Lumina.Excel.GeneratedSheets;
-using System;
+﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
+using Dalamud.Game.ClientState.Objects.SubKinds;
+using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.Game.ClientState.Resolvers;
+using Dalamud.Game.Text.SeStringHandling;
+using ECommons.DalamudServices;
+using ECommons.ExcelServices;
+using Lumina.Excel.GeneratedSheets;
 
 namespace ECommons.ChatMethods;
 #nullable disable
@@ -13,7 +16,25 @@ namespace ECommons.ChatMethods;
 public struct Sender : IEquatable<Sender>
 {
     public string Name;
+
     public uint HomeWorld;
+
+    public static bool TryParse(string nameWithWorld, out Sender s)
+    {
+        string[] array = nameWithWorld.Split('@');
+        if (array.Length == 2)
+        {
+            World world = ExcelWorldHelper.Get(array[1]);
+            if (world != null)
+            {
+                s = new Sender(array[0], world.RowId);
+                return true;
+            }
+        }
+
+        s = default(Sender);
+        return false;
+    }
 
     public Sender(string Name, uint HomeWorld)
     {
@@ -22,42 +43,55 @@ public struct Sender : IEquatable<Sender>
     }
 
     public Sender(SeString Name, uint HomeWorld)
+        : this(Name.ToString(), HomeWorld)
     {
-        this = new(Name.ToString(), HomeWorld);
     }
 
     public Sender(SeString Name, Dalamud.Game.ClientState.Resolvers.ExcelResolver<World> HomeWorld)
+        : this(Name.ToString(), HomeWorld.Id)
     {
-        this = new(Name.ToString(), HomeWorld.Id);
     }
 
     public Sender(string Name, Dalamud.Game.ClientState.Resolvers.ExcelResolver<World> HomeWorld)
+        : this(Name, HomeWorld.Id)
     {
-        this = new(Name, HomeWorld.Id);
     }
 
     public Sender(IPlayerCharacter pc)
+        : this(pc.Name, pc.HomeWorld)
     {
-        this = new(pc.Name, pc.HomeWorld);
     }
 
     public override bool Equals(object obj)
     {
-        return obj is Sender sender && Equals(sender);
+        if (obj is Sender other)
+        {
+            return Equals(other);
+        }
+
+        return false;
     }
 
     public bool Equals(Sender other)
     {
-        return Name == other.Name &&
-               HomeWorld == other.HomeWorld;
+        if (Name == other.Name)
+        {
+            return HomeWorld == other.HomeWorld;
+        }
+
+        return false;
     }
 
     public IPlayerCharacter? Find()
     {
-        foreach(var x in Svc.Objects)
+        foreach (IGameObject @object in Svc.Objects)
         {
-            if(x is IPlayerCharacter pc && pc.Name.ToString() == Name && pc.HomeWorld.Id == HomeWorld) return pc;
+            if (@object is IPlayerCharacter playerCharacter && playerCharacter.Name.ToString() == Name && playerCharacter.HomeWorld.Id == HomeWorld)
+            {
+                return playerCharacter;
+            }
         }
+
         return null;
     }
 
